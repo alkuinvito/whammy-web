26e7ab7f7b7f1585fc99313b4a7139ba9c561ebe
alkuinvito <alkuinvito@tutanota.com>
2025-07-24 12:33:30 +0700

    chore: remove debug logging

--- src/components/Commands/ClearCommand.tsx
+++ src/components/Commands/ClearCommand.tsx
@@ -3,7 +3,6 @@ import type { CommandParamsProps } from "./CommandHandler";
 function ClearCommand({ params }: CommandParamsProps) {
   if (params) {
     const args = params.shift();
-    console.log(params, args);
     if (args) {
       return `Invalid args: ${args}. Type \`help clear\` for usage`;
     }
